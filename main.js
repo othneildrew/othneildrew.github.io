3f8236fd8c6c8cc5944eee697e873d4e89d57faf
Othneil I Drew <codeguydrew@gmail.com>
2018-09-25 11:42:09 -0500

    Fixed navbar text color change issue on scroll

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ $(function() {
   // Add background color to nav when user scrolls more than 50px down
   $(window).scroll(function() {
     if ($(this).scrollTop() > 50) {
-      $('#main-navbar').addClass('scrolledNavbar');
+      $('#main-navbar').removeClass('navbar-dark');
+      $('#main-navbar').addClass('scrolledNavbar navbar-light');
     }
     if ($(this).scrollTop() < 50) {
-      $('#main-navbar').removeClass('scrolledNavbar');
+      $('#main-navbar').removeClass('scrolledNavbar navbar-light');
+      $('#main-navbar').addClass('navbar-dark');
     }
   });
 
